a48aa9f4d1ce14c0d03dd3aa583155bdbbb5f86a
enaena <e_ngu_no1@yahoo.co.jp>
2020-11-24 19:41:50 +0900

    バリテーション_02

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, inclusion: { in: "@" }
+
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+
   validates :birthday, presence: true
 end
